c12e1fd16a34baad4fbb6fd28067d110e461f059
mikeyy2 <adhikari.shrish1@gmail.com>
2023-01-14 16:14:28 +0545

    Added template skeleton, flask_moment, flask_bootstrap to make design decisions simpler

--- hello.py
+++ hello.py
@@ -1,14 +1,19 @@
 from flask import Flask, render_template
 from flask_bootstrap import Bootstrap
+from flask_moment import Moment
 
+from datetime import datetime
 
 
 app = Flask(__name__)
-Bootstrap = Bootstrap(app)
+bootstrap = Bootstrap(app)
+moment = Moment(app)
+
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    return render_template("index.html", current_time=datetime.utcnow())
+
 
 @app.route("/user/<name>")
 def user(name):
@@ -24,9 +29,11 @@ def user(name):
 def page_not_found(e):
     return render_template("404.html"), 404
 
+
 @app.errorhandler(500)
 def internal_server_error(e):
     return render_template("500.html"), 500
+
 
 if __name__ == "__main__":
     app.run(debug=True)
