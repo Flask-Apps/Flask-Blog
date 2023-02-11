98be3fd79f8ab5c93c884aa21e01c05cb46d5494
mikeyy2 <adhikari.shrish1@gmail.com>
2023-02-11 14:02:23 +0545

    Added user profile fields into user model

--- app/models.py
+++ app/models.py
@@ -3,6 +3,7 @@ from flask import current_app
 from flask_login import UserMixin, AnonymousUserMixin
 from werkzeug.security import generate_password_hash, check_password_hash
 from itsdangerous import URLSafeTimedSerializer as Serializer
+from datetime import datetime
 
 
 class Permission:
@@ -96,6 +97,13 @@ class User(UserMixin, db.Model):
     email = db.Column(db.String(64), unique=True, index=True)
     password_hash = db.Column(db.String(128))
     confirmed = db.Column(db.Boolean, default=False)
+
+    # for user profile
+    name = db.Column(db.String(64))
+    location = db.Column(db.String(64))
+    about_me = db.Column(db.Text())
+    memeber_since = db.Column(db.DateTime(), default=datetime.utcnow)
+    last_seen = db.Column(db.DateTime(), default=datetime.utcnow)
 
     def __init__(self, **kwargs) -> None:
         super(User, self).__init__(**kwargs)
