d77525be20f2d3f7320fe5a09d3047417452ca20
mikeyy2 <adhikari.shrish1@gmail.com>
2023-02-13 10:28:33 +0545

    Added form for admin ti  edit user profile

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,6 +1,19 @@
-from wtforms import StringField, SubmitField, TextAreaField
-from wtforms.validators import DataRequired, Length
+from wtforms import (
+    StringField,
+    SubmitField,
+    TextAreaField,
+    BooleanField,
+    SelectField,
+)
+from wtforms.validators import (
+    DataRequired,
+    Length,
+    Email,
+    Regexp,
+    ValidationError,
+)
 from flask_wtf import FlaskForm
+from ..models import Role, User
 
 
 class NameForm(FlaskForm):
@@ -18,3 +31,53 @@ class EditProfileForm(FlaskForm):
     location = StringField("Location", validators=[Length(0, 64)])
     about_me = TextAreaField("About Me")
     submit = SubmitField("Submit")
+
+
+class EditProfileAdminForm(FlaskForm):
+    email = StringField(
+        "Email", validators=[DataRequired(), Length(0, 64), Email()]
+    )
+    username = StringField(
+        "Username",
+        validators=[
+            DataRequired(),
+            Length(1, 64),
+            Regexp(
+                "^[A-Za-z][A-Za-z0-9_.]*$",
+                0,  # no special behaviour or options are being set
+                # like re.IGNORECASE, re.MULTILINE, etc
+                "Usernames must have only letters, numbers, dots or"
+                "underscores",
+            ),
+        ],
+    )
+    confirmed = BooleanField("Confirmed")
+    # coerce convert the user i/p (str) to int
+    # user selected value will be converted to int
+    role = SelectField("Role", coerce=int)
+    name = StringField("Real Name", validators=[Length(0, 64)])
+    location = StringField("Location", validators=[Length(0, 64)])
+    about_me = TextAreaField("About Me")
+    submit = SubmitField("Submit")
+
+    def __init__(self, user, *args, **kwargs):
+        super(EditProfileAdminForm, self).__init__(*args, **kwargs)
+        self.role.choices = [
+            (role.id, role.name)
+            for role in Role.query.order_by(Role.name).all()
+        ]
+        self.user = user
+
+    def validate_email(self, field):
+        if (
+            field.data != self.user.email
+            and User.query.filter_by(email=field.data).first()
+        ):
+            raise ValidationError("Email already registered.")
+
+    def validate_username(self, field):
+        if (
+            field.data != self.user.username
+            and User.query.filter_by(username=field.data).first()
+        ):
+            raise ValidationError("Username already in use.")
