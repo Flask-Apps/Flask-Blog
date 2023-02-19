3b06b0167560fd2e5a6dc46e6f49cd3b160dab0e
mikeyy2 <adhikari.shrish1@gmail.com>
2023-02-19 22:39:45 +0545

    Added posts edit functionality for post owner and for the admin

--- app/main/views.py
+++ app/main/views.py
@@ -1,4 +1,4 @@
-from flask import render_template, redirect, url_for, flash, current_app, request
+from flask import render_template, redirect, url_for, flash, current_app, request, abort
 from flask_login import login_required, current_user
 from .forms import EditProfileForm, EditProfileAdminForm, PostForm
 from .. import db
@@ -84,6 +84,28 @@ def edit_profile_admin(id):
     form.location.data = user.location
     form.about_me.data = user.about_me
     return render_template("edit_profile.html", form=form, user=user)
+
+
+@main.route("/posts/<int:id>")
+def post(id):
+    post = Post.query.get_or_404(id)
+    return render_template("post.html", posts=[post])
+
+
+@main.route("/edit/<int:id>", methods=["GET", "POST"])
+def edit(id):
+    post = Post.query.get_or_404(id)
+    if current_user != post.author and not current_user.can(Permission.ADMIN):
+        abort(403)
+    form = PostForm()
+    if form.validate_on_submit():
+        post.body = form.body.data
+        db.session.add(post)
+        db.session.commit()
+        flash("The post  has been updated.")
+        return redirect(url_for(".post", id=post.id))
+    form.body.data = post.body
+    return render_template("edit_post.html", form=form)
 
 
 # @main.route("/user/<name>")
