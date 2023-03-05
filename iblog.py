16b4ded122fe0eb80174f8b02a2e03fe240f1cb3
mikeyy2 <adhikari.shrish1@gmail.com>
2023-03-05 09:38:55 +0545

    Added test coverage

--- iblog.py
+++ iblog.py
@@ -1,4 +1,6 @@
 import os
+import sys
+import click
 from app import create_app, db
 from app.models import User, Role, Post
 from flask_migrate import Migrate
@@ -9,6 +11,12 @@ load_dotenv()
 
 
 app = create_app(os.getenv("FLASK_CONFIG", "default"))
+COV = None
+if os.environ.get("IBLOG_COVERAGE"):
+    import coverage
+
+    COV = coverage.coverage(branch=True, include="app/*")
+    COV.start()
 
 # adds flask db command with several subcommands
 migrate = Migrate(app, db)
@@ -23,13 +31,39 @@ def make_shell_context():
     return dict(db=db, User=User, Role=Role, Post=Post)
 
 
+# @app.cli.command()
+# def test():
+#     """Run the unit tests."""
+#     import unittest
+
+#     tests = unittest.TestLoader().discover("tests")
+#     unittest.TextTestRunner(verbosity=2).run(tests)
+
+
 @app.cli.command()
-def test():
+@click.option(
+    "--coverage/--no-coverage", default=False, help="Run tests under code coverage"
+)
+def test(coverage):
     """Run the unit tests."""
+    if coverage and not os.environ.get("IBLOG_COVERAGE"):
+        os.environ["IBLOG_COVERAGE"] = "1"
+        # restart the script with new option
+        os.execv(sys.executable, [sys.executable] + sys.argv)
     import unittest
 
     tests = unittest.TestLoader().discover("tests")
     unittest.TextTestRunner(verbosity=2).run(tests)
+    if COV:
+        COV.stop()
+        COV.save()
+        print("Coverage Summary:")
+        COV.report()
+        basedir = os.path.abspath(os.path.dirname(__file__))
+        covdir = os.path.join(basedir, "tmp/coverage")
+        COV.html_report(directory=covdir)
+        print("HTML version: file://%s/index.html" % covdir)
+        COV.erase()
 
 
 @app.route("/secret")
