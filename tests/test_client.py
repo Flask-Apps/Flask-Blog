a8739cd98fb1cf5d8934949ce9baf81c3de3d90d
mikeyy2 <adhikari.shrish1@gmail.com>
2023-03-07 15:43:03 +0545

    Added more tests to client

--- tests/test_client.py
+++ tests/test_client.py
@@ -1,4 +1,5 @@
 import unittest
+import re
 from app import create_app, db
 from app.models import User, Role
 
@@ -21,3 +22,45 @@ class FlaskClientTestCase(unittest.TestCase):
         response = self.client.get("/")
         self.assertEqual(response.status_code, 200)
         self.assertTrue("Stranger" in response.get_data(as_text=True))
+
+    def test_register_and_login(self):
+        # register a new account
+        response = self.client.post(
+            "/auth/register",
+            data={
+                "email": "joe@example.com",
+                "username": "joe",
+                "password": "cat",
+                "password2": "cat",
+            },
+        )
+        self.assertEqual(response.status_code, 302)
+
+        # login with the new account
+        response = self.client.post(
+            "/auth/login",
+            data={"email": "joe@example.com", "password": "cat"},
+            follow_redirects=True,
+        )
+        self.assertEqual(response.status_code, 200)
+        self.assertTrue(re.search("Hello,\s+joe!", response.get_data(as_text=True)))
+        self.assertTrue(
+            "You have not confirmed your account yet" in response.get_data(as_text=True)
+        )
+
+        # send a confirmation token
+        user = User.query.filter_by(email="joe@example.com").first()
+        token = user.generate_confirmation_token()
+        response = self.client.get(
+            "/auth/confirm/{}".format(token), follow_redirects=True
+        )
+        user.confirm(token)
+        self.assertEqual(response.status_code, 200)
+        self.assertTrue(
+            "You have confrimed your account" in response.get_data(as_text=True)
+        )
+
+        # log out
+        response = self.client.get("/auth/logout", follow_redirects=True)
+        self.assertEqual(response.status_code, 200)
+        self.assertTrue("You have been logged out" in response.get_data(as_text=True))
